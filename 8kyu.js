e33c24bc249df83662e27d594aeb0a03c96239fe
laurenhatchett <laurenhatchett@gmail.com>
2023-02-19 09:21:47 -0500

    solve find smallest integer problem

--- 8kyu.js
+++ 8kyu.js
@@ -36,3 +36,14 @@ smash = function (words) {
   return words.join(" ")
 }
 
+/* Give an array of integers your solution should find the smallest integer.
+For example:
+          -Given [34, 15, 88, 2] your solution will return 2
+          -Given [34, -345, -1, 100] your solution will return -345
+
+  You can assume, for the purpose of this kata, that the supplied array will not be empty. */
+
+findSmallestInt(args) {
+  return Math.min(...args)
+}
+
